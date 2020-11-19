d0aefb3e850804e18687543fcb0ca9d2852be589
johnmmason <jmmason@utexas.edu>
2020-11-18 18:45:41 -0600

    rfe model

--- Jupyter/mvp3/stepwise.py
+++ Jupyter/mvp3/stepwise.py
@@ -1,45 +1,65 @@
 import numpy as np
 import pandas as pd
+
 from floridaman import data_cleaning
+
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import RFE
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import plot_confusion_matrix
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
 
-n_estimators = [int(x) for x in np.linspace(start = 4, stop = 10, num = 10)]
-max_features = ['auto', 'sqrt']
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-min_samples_split = [2, 5, 10]
-min_samples_leaf = [1, 2, 4]
-bootstrap = [True, False]
+def train(X_train, y_train, X_test, y_test):
+    # Create KNN Imputer object
+    imputer = KNNImputer()
 
-random_grid = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
+    # Create scaler object
+    scaler = StandardScaler()
 
-def train_model(df, threshold, columns):
-    candidate_data = data_cleaning.generate_candidate_dataset(df, threshold, threshold, 5)
-    train_data = data_cleaning.balance(candidate_data)
+    # Create Random Forest Classifier object
+    classifier = RandomForestClassifier()
 
-    X_train = np.array(train_data[columns])
-    y_train = np.array(train_data['FAILURETYPE'])
-    X_test = np.array(candidate_data[columns])
-    y_test = np.array(candidate_data['FAILURETYPE'])
+    # Create RFE object
+    rfe_classifier = LogisticRegression(max_iter=100, tol=0.1)
+    rfe = RFE(rfe_classifier)
 
-    rf_model = RandomForestClassifier()
-    rf_grid = GridSearchCV(estimator = rf_model, param_grid = random_grid, cv = 8, verbose = 1, n_jobs = -1)
-    rf_grid.fit(X_train, y_train)
-    
-    best_model = rf_grid.best_estimator_
+    # Combine components into one Pipeline object
+    pipeline = Pipeline( steps=[('imputer', imputer), ('scaler', scaler), ('rfe', rfe), ('classifier', classifier)] )
 
-    return f1_score(y_test, best_model.predict(X_test), average='weighted')
+    # Create parameter grid for GridSearchCV
+    param_grid = {
+        'imputer__n_neighbors': [5],
+        'rfe__n_features_to_select': [3, 5, 10, 15, 20, 25],
+        'classifier__n_estimators': [int(x) for x in np.linspace(start = 4, stop = 10, num = 4)],
+        'classifier__max_features': ['auto', 'sqrt'],
+        'classifier__max_depth': [int(x) for x in np.linspace(10, 110, num = 11)],
+        'classifier__min_samples_split': [2, 5, 10],
+        'classifier__min_samples_leaf': [1, 2, 4],
+        'classifier__bootstrap': [True, False]
+    }
+
+    # Create and train GridSearchCV object
+    gs = GridSearchCV(pipeline, param_grid, verbose=1, n_jobs=-1) # create
+    gs.fit(X_train, y_train) # train
+
+    return gs
 
 if __name__ == "__main__":
     raw_data = data_cleaning.load('null_transformed')
+    candidate_data = data_cleaning.drop_columns(raw_data, .4, .4)
+    train_data = data_cleaning.balance(candidate_data)
 
-    print( train_model(raw_data, .4, ['PrimarySetpoint','SecondarySetpoint']) )
+    X_train = np.array(train_data[data_cleaning.features(train_data)])
+    y_train = np.array(train_data['FAILURETYPE'])
+
+    X_test = np.array(candidate_data[data_cleaning.features(train_data)])
+    y_test = np.array(candidate_data['FAILURETYPE'])
+
+    results = train(X_train, y_train, X_test, y_test)
+
+    print(str( classification_report(y_test, results.best_estimator_.predict(X_test)) ) )
+    print('')
+    print(results.best_params_)
